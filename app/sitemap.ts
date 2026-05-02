6430b7d3ddfa95088349ecf3953cc07472113148
rahul@construction.live <rahul@construction.live>
2026-05-02 15:15:10 -0700

    logo and svg fixed

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-	const baseUrl = "https://constructionlive.com";
+	const baseUrl = "https://construction.live";
 
 	return [
 		{
